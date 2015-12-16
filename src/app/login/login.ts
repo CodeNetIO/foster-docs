54b170d7d919eb8ae9951731e0fd65ca845ace31
darinh <darin@codenet.io>
2015-12-16 06:50:42 -0800

    Color changes, added login folder.

---
+++ src/app/login/login.ts
@@ -0,0 +1,2 @@
+/// <reference path="../../../typings/app.d.ts" />
+import {Component, View} from 'angular2/angular2';
--- typings/app.d.ts
+++ typings/app.d.ts
@@ -4,23 +4,23 @@
 
 /// <reference path="../src/app/dashboard/dashboard.ts" />
 
-/// <reference path="../src/app/menu/menu.ts" />
-
 /// <reference path="../src/app/extensions/date.ts" />
-
-/// <reference path="../src/app/statusBar/statusBar.ts" />
 
 /// <reference path="../src/app/model/menuItem.ts" />
 
 /// <reference path="../src/app/model/todoItem.ts" />
 
+/// <reference path="../src/app/menu/menu.ts" />
+
 /// <reference path="../src/app/todo/todo.ts" />
+
+/// <reference path="../src/app/statusBar/statusBar.ts" />
 
 /// <reference path="../src/app/dashboard/todoWidget/todoWidget.ts" />
 
-/// <reference path="../src/app/menu/menuItem/menuItem.ts" />
-
 /// <reference path="../src/app/model/enums/priority.ts" />
+
+/// <reference path="../src/app/menu/menuItem/menuItem.ts" />
 
 /// <reference path="../src/app/todo/todoItem/todoItem.ts" />
 
